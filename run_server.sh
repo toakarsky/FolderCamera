0a675cdcc23ea08bb0a90993aaa8849fd62e1e6f
toakarsky <60446065+toakarsky@users.noreply.github.com>
2020-03-12 13:19:33 +0100

    Better Way

--- run_server.sh
+++ run_server.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
                                                   #ADRESIP:PORT
-gunicorn --worker-class gevent --workers 1 --bind 0.0.0.0:1935 folderCamera:app
+gunicorn --threads 5 --workers 1 --bind 0.0.0.0:1935 folderCamera:app
